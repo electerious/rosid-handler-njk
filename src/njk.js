019e78caec3376bea3e8bac9bf5b23b3ad840db6
Tobias Reich <tobias.reich.ich@gmail.com>
2017-02-07 15:44:45 +0100

    Fixed path. Must be dir of filePath

--- src/njk.js
+++ src/njk.js
@@ -1,6 +1,7 @@
 'use strict'
 
-const njk = require('nunjucks')
+const path = require('path')
+const njk  = require('nunjucks')
 
 /*
  * Transform Nunjucks to HTML.
@@ -12,8 +13,11 @@ module.exports = function(filePath, data) {
 
 	return new Promise((resolve, reject) => {
 
+		// Get the directory of filePath
+		const dirPath = path.dirname(filePath)
+
 		// Configure Nunjucks
-		njk.configure(filePath)
+		njk.configure(dirPath)
 
 		// Render Nunjucks
 		const result = njk.render(filePath, data, (err, str) => {
